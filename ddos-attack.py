2f3ba329b5e1a5ea4fe90318e324177c1801b75b
Sumon SN <56505303+sumonsn@users.noreply.github.com>
2020-06-30 12:40:44 +0000

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,9 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : Sumon-SN"
+print "github   : https://github.com/sumonsn"
+print 
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
